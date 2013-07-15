211e0eb1ab301195208d80969ff41b0814610a5a
OpenShift guest <romnicktibod@gmail.com>
2013-07-15 06:29:52 -0400

    Bugfix on remote server application migration

--- add_score.php
+++ add_score.php
@@ -1,21 +1,35 @@
 <?php
-	$con=mysqli_connect("localhost","root","", "score");
-	
-	if (mysqli_connect_errno())
-	{
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-	}
 
+    $services_json = json_decode(getenv("VCAP_SERVICES"),true);
+    $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+    $username = $mysql_config["username"];
+    $password = $mysql_config["password"];
+    $hostname = $mysql_config["hostname"];
+    $port = $mysql_config["port"];
+    $db = $mysql_config["name"];
+
+    // Create connection
+    $con=mysqli_connect($hostname, $username, $password, $db,$port);
+    // Check connection
+    if (mysqli_connect_errno($con))
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
 	
 	$user=$_POST["user"]; 
 	$duration=$_POST["duration"]; 
 	$score=$_POST["score"]; 
 	$location=$_POST["location"]; 
 
-
-	mysqli_query($con,"INSERT INTO game_score (user, duration, score, location)
-	VALUES ('". $user ."', ". $duration .", ". $score .", '". $location ."')");
+    $query = "INSERT INTO game_score (user, duration, score, location)
+    VALUES ('". $user ."', ". $duration .", ". $score .", '". $location ."')";
+    echo var_dump($query);
+	$result = mysqli_query($con,$query);
 	
+    echo var_dump($result);
+    
+    echo mysqli_error();
+    
 	echo $user;
 	mysqli_close($con);
 	
--- view_score.php
+++ view_score.php
@@ -1,10 +1,38 @@
 <?php
-
-	$con=mysqli_connect("localhost","root","","score");
-	$result = mysqli_query($con,"SELECT TOP 10 * FROM game_score ORDER BY duration, score");
-	$output = mysqli_fetch_array($result);
 	
-	print(json_encode($output));
+    $services_json = json_decode(getenv("VCAP_SERVICES"),true);
+    $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
+    $username = $mysql_config["username"];
+    $password = $mysql_config["password"];
+    $hostname = $mysql_config["hostname"];
+    $port = $mysql_config["port"];
+    $db = $mysql_config["name"];
+
+    // Create connection
+    $con=mysqli_connect($hostname, $username, $password, $db,$port);
+    // Check connection
+    if (mysqli_connect_errno($con))
+    {
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    }
+    $result = mysqli_query($con,"SELECT * FROM game_score ORDER BY duration, score LIMIT 0,10");
+    
+?>
+    <table>
+        <tbody>
+<?php
+    while($row = mysqli_fetch_array($result))
+    {
+        echo "<tr>";
+        foreach($row as $r)
+            echo "<td>".$r."</td>";
+        echo "</tr>";
+    }
+?>
+        </tbody>
+    </table>
+<?php
+    print(json_encode($output));
 	
 	mysqli_close($con);
 	
